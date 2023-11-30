8dbb2a4f0702d479f85da40a0842fabf5f6af83e
Daniel Harding <danielharding2008@gmail.com>
2023-11-30 20:12:34 +0000

    Unworking code

--- assets/script.js
+++ assets/script.js
@@ -6,20 +6,19 @@ tt.map({
 
 var APIKey = "1";
 var queryURL =
+  "https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita&appid=" +
+  APIKey;
+var time = 0;
+
+var APIKey = "1";
+var queryURL =
   "https://thecocktaildb.com/api/json/v1/1/filter.php?i=Gin&appid=" + APIKey;
 
 fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(queryURL);
-        console.log(data);
-    })
-
-
-    document.querySelector('#')
-    $("#ingredient-submit").on("submit", function() {
-        var ingredient = $("#ingredient").val().trim;
-        console.log(ingredient);
-    });
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(queryURL);
+    console.log(data);
+  });
