1e474fe160d948c283cd84599e219ef0b69da5e9
Daniel Harding <danielharding2008@gmail.com>
2023-11-30 19:25:38 +0000

    working on the map feature

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,19 @@
-var time = 0;
-
+tt.setProductInfo("Whats Near You", "V1");
+tt.map({
+  key: "qyRVte5GUbZjU2y2Z9LftPqp8OBwVPoa",
+  container: "map",
+});
 
 var APIKey = "1";
-var queryURL = "https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita&appid=" + APIKey;
+var queryURL =
+  "https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita&appid=" +
+  APIKey;
 
 fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(queryURL);
-        console.log(data);
-    })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(queryURL);
+    console.log(data);
+  });
