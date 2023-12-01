64b8d19d60dd54d7c170039094bf524b0b829f1f
Daniel Harding <danielharding2008@gmail.com>
2023-12-01 18:24:52 +0000

    added search funactiality

--- assets/script1.js
+++ assets/script1.js
@@ -4,6 +4,22 @@ tt.map({
   container: "map",
 });
 
+var options = {
+  searchOptions: {
+    key: "qyRVte5GUbZjU2y2Z9LftPqp8OBwVPoa",
+    language: "en-GB",
+    limit: 5,
+  },
+  autocompleteOptions: {
+    key: "qyRVte5GUbZjU2y2Z9LftPqp8OBwVPoa",
+    language: "en-GB",
+  },
+};
+
+var ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
+var searchBoxHTML = ttSearchBox.getSearchBoxHTML();
+$("#map").append(searchBoxHTML);
+
 var APIKey = "1";
 var queryURL =
   "https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita&appid=" +
