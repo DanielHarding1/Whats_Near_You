abd3926307c23ade94eac8d9086f687741a1520b
Chelsea <chelsea.chapman4@hotmail.com>
2023-12-01 19:49:17 +0000

    adds user input to the API key

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,24 @@ tt.map({
   container: "map",
 });
 
+$("#ingredient-submit").on("click", function (event) {
+  event.preventDefault();
+  var ingredient = $("#ingredient").val();
+  console.log(ingredient);
+
+
 var APIKey = "1";
 var queryURL =
-  "https://thecocktaildb.com/api/json/v1/1/filter.php?i=Gin&appid=" + APIKey;
+  "https://thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient + "&appid=" + APIKey;
 
+console.log(queryURL);
+});
 fetch(queryURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(queryURL);
     console.log(data);
   });
 
-$("#ingredient-submit").on("click", function (event) {
-  event.preventDefault();
-  var ingredient = $("#ingredient").val().trim();
-  console.log(ingredient);
-});
+
