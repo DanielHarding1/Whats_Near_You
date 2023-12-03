3025fcd742b892c6052007fb55cfad82b01159c1
Chelsea <chelsea.chapman4@hotmail.com>
2023-12-03 09:52:51 +0000

    Adds search function to locate cocktail by name

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,47 @@
+var APIKey = "1";
+
+// Searches for a cocktail by name
+$("#cocktail-submit").on("click", function (event) {
+  event.preventDefault();
+  var cocktail = $("#cocktail").val();
+  console.log(cocktail);
+
+  var cocktailQueryURL = "https://thecocktaildb.com/api/json/v1/1/search.php?s=" +
+  cocktail +
+  "&appid=" +
+  APIKey;
+
+  console.log(cocktailQueryURL);
+
+  fetch(cocktailQueryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+});
+
+// Searches for cocktail by ingredeitn
+// TO DO: WORK OUT HOW TO MAKE THE ALCOHOLIC OPTIONS SELECT BUTTONS INSTEAD OF SEARCH ITEMS
 $("#ingredient-submit").on("click", function (event) {
   event.preventDefault();
   var ingredient = $("#ingredient").val();
   console.log(ingredient);
 
+  var ingredientQueryURL =
+    "https://thecocktaildb.com/api/json/v1/1/filter.php?i=" +
+    ingredient +
+    "&appid=" +
+    APIKey;
 
-var APIKey = "1";
-var queryURL =
-  "https://thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient + "&appid=" + APIKey;
+  console.log(ingredientQueryURL);
 
-console.log(queryURL);
-
-fetch(queryURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+  fetch(ingredientQueryURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 });
-
